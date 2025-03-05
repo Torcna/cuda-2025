54a2d1ece4d2942bb9b3cdcb945e07a6797631b6
Mironov Arseniy <arseniy.mironov.01@mail.ru>
2025-03-05 20:37:27 +0300

    first commit

---
+++ 3822B1FI1/2_gelu_cuda/mironov_arseniy/gelu_cuda.cu
@@ -0,0 +1,32 @@
+﻿#include "gelu_cuda.h"
+#include <cmath>
+#include <cuda_runtime.h>
+
+#define PI 0.636619f
+#define BLOCK_SIZE 256
+
+__global__ void GeluKernel(const float* in, float* out, int size) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < size) {
+        float x = in[idx];
+        out[idx] = 0.5f * x * (1.f + tanh(PI * (x + 0.044715f * (x * x * x))));
+    }
+}
+
+__host__ std::vector<float> GeluCUDA(const std::vector<float>& input) {
+    int size = input.size(), memory = size * sizeof(float);
+    float* in, *out;
+	std::vector<float> result(size);
+
+    cudaMalloc(&in, memory);
+    cudaMalloc(&out, memory);
+
+    cudaMemcpy(in, input.data(), memory, cudaMemcpyKind::cudaMemcpyHostToDevice);
+    GeluKernel<<<(size + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE>>> (in, out, size);
+    cudaMemcpy(result.data(), out, memory, cudaMemcpyKind::cudaMemcpyDeviceToHost);
+
+    cudaFree(in);
+    cudaFree(out);
+
+    return result;
+}
