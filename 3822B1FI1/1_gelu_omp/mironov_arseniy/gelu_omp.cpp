5896d5fd0bfd45e5f55c445befbdb6fc6ba81dbc
Mironov Arseniy <arseniy.mironov.01@mail.ru>
2025-03-05 15:36:58 +0300

    add std::

--- 3822B1FI1/1_gelu_omp/mironov_arseniy/gelu_omp.cpp
+++ 3822B1FI1/1_gelu_omp/mironov_arseniy/gelu_omp.cpp
@@ -7,9 +7,9 @@ AlignedVector GeluOMP(const AlignedVector& input) {
 	
 	AlignedVector result(input.size());
 	#pragma omp parallel for
-	for (size_t it = 0; it < input.size(); ++it) {
+	for (std::size_t it = 0; it < input.size(); ++it) {
 		float x = input[it];
-		result[it] = 0.5f * x * (1.f + tanh(PI * (x + 0.044715f * (x * x * x))));
+		result[it] = 0.5f * x * (1.f + std::tanh(PI * (x + 0.044715f * (x * x * x))));
 	}
 	return result;
 }
