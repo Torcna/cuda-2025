70b985f55070f3061b6701692ecaaeed1e31bfe9
Aleksey Baranov <ale.baranov@yadro.com>
2025-03-09 15:30:42 +0300

    init

---
+++ 3822B1FI1/1_gelu_omp/baranov_aleksey/gelu_omp.cpp
@@ -0,0 +1,20 @@
+#include "gelu_omp.h"
+#include <cmath>
+#include <omp.h>
+
+AlignedVector GeluOMP(const AlignedVector& input) {
+    AlignedVector result(input.size());
+    const float precalc_sqrt = std::sqrt(2.0f / M_PI);
+	float x =0;
+    float x3=0;
+    float arg=0;
+
+    #pragma omp parallel for
+    for (size_t i = 0, sz= input.size(); i !=sz ; ++i) {
+        x = input[i];
+        x3 = x * x * x;
+        arg = precalc_sqrt * (x + 0.044715f * x3);
+        result[i] = 0.5f * x * (1.0f + std::tanh(arg));
+    }
+    return result;
+}
