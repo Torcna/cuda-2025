f9cdace39d99578c8d38d3c377fd3a00868a35d8
Mironov Arseniy <arseniy.mironov.01@mail.ru>
2025-03-20 17:16:05 +0300

    change transpose

--- 3822B1FI1/7_gemm_cublas/mironov_arseniy/gemm_cublas.cu
+++ 3822B1FI1/7_gemm_cublas/mironov_arseniy/gemm_cublas.cu
@@ -2,17 +2,19 @@
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 
+
 std::vector<float> GemmCUBLAS(const std::vector<float>& a,
                               const std::vector<float>& b,
                               int n) {
     int memory = n * n * sizeof(float);
     int square = n * n;
-    float* in1, *in2, *out;
+    float* in1, *in2, *out, *out_transposed;
 	std::vector<float> result(square);
     
     cudaMalloc(&in1, memory);
     cudaMalloc(&in2, memory);
     cudaMalloc(&out, memory);
+    cudaMalloc(&out_transposed, memory);
 
     cudaMemcpy(in1, a.data(), memory, cudaMemcpyHostToDevice);
     cudaMemcpy(in2, b.data(), memory, cudaMemcpyHostToDevice);
@@ -22,17 +24,13 @@ std::vector<float> GemmCUBLAS(const std::vector<float>& a,
     float alpha = 1.0f, beta= 0.0f;
 
     cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, n, n, n, &alpha, in1, n, in2, n, &beta, out, n);
-
-    cudaMemcpy(result.data(), out, memory, cudaMemcpyDeviceToHost);
+    cublasSgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, n, n, &alpha, out, n, &beta, nullptr, n, out_transposed, n);
+    cudaMemcpy(result.data(), out_transposed, memory, cudaMemcpyDeviceToHost);
     
-    for(int i = 0; i < n; ++i){
-        for(int j = i + 1; j < n; ++j){
-            std::swap(result[i * n + j], result[j * n + i]);
-        }
-    }
     cudaFree(in1);
     cudaFree(in2);
     cudaFree(out);
+    cudaFree(out_transposed);
     cublasDestroy(handle);
     return result;
 }
