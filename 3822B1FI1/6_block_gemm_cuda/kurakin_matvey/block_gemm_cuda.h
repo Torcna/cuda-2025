8fd90dcc04b34fb4dd1861a18e0601d5bb22b491
Matvej Kurakin <matvejkurakin7755@gmail.com>
2025-03-21 09:31:28 +0300

    6_block_gemm_cuda task

---
+++ 3822B1FI1/6_block_gemm_cuda/kurakin_matvey/block_gemm_cuda.h
@@ -0,0 +1,11 @@
+#ifndef __BLOCK_GEMM_CUDA_H
+#define __BLOCK_GEMM_CUDA_H
+
+#pragma once
+#include <cuda_runtime.h>
+
+#include <vector>
+
+std::vector<float> BlockGemmCUDA(const std::vector<float> &a, const std::vector<float> &b, int n);
+
+#endif  // __BLOCK_GEMM_CUDA_H
