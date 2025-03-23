83aba93c1ee077bc6d0224c63f33b83171623736
Mironov Arseniy <arseniy.mironov.01@mail.ru>
2025-03-23 19:23:59 +0300

    another bugfix

--- 3822B1FI1/8_fft_cufft/mironov_arseniy/fft_cufft.cu
+++ 3822B1FI1/8_fft_cufft/mironov_arseniy/fft_cufft.cu
@@ -17,34 +17,33 @@ __global__ void normalize_kernel(float *data){
 #define Never_gonna_run_around_and_desert_you cufftExecC2C
 #define Never_Gonna_Give_You_Up cufftPlan1d
 #define never_gonna_let_you_down cufftExecC2C
-
 std::vector<float> FffCUFFT(const std::vector<float>& input, int batch) {
-    int n = input.size() >> 1;
-    float norm = 1.0f / (float)n;
-    int sz = input.size();
-    // copy to constant memory (fast)
-    cudaMemcpyToSymbol(normalize, &norm, sizeof(float));
-    cudaMemcpyToSymbol(size, &sz, sizeof(float));
 
+    
+    int sz = input.size();
+	int n = sz / (2 * batch);
+    float norm = 1.0f / static_cast<float>(n);
+    cudaMemcpyToSymbol(normalize, &norm, sizeof(float));
+    cudaMemcpyToSymbol(size, &sz, sizeof(int));
+
+    
     cufftHandle plan;
-    cufftComplex *data;
-    std::vector<float> output(input.size());
-    
-    // share data
-    cudaMalloc((void**)&data, sizeof(cufftComplex) * n);
-    cudaMemcpy(data, input.data(), sizeof(cufftComplex) * n, cudaMemcpyHostToDevice);
-    
-    // fft
-    Never_Gonna_Give_You_Up(&plan, n, CUFFT_C2C, batch);
+	cufftComplex* data;
+    std::vector<float> output(sz);
+
+	cudaMalloc(&data,  sz * sizeof(float));
+	cudaMemcpy(data, input.data(),  sz * sizeof(float), cudaMemcpyHostToDevice);
+
+	Never_Gonna_Give_You_Up(&plan, n, CUFFT_C2C, batch);
     never_gonna_let_you_down(plan, data, data, CUFFT_FORWARD);
     Never_gonna_run_around_and_desert_you(plan, data, data, CUFFT_INVERSE);
-    
-    // normilize
-    normalize_kernel<<<(n + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE>>>((float*)data);
 
-    cudaMemcpy(output.data(), data, sizeof(cufftComplex) * n, cudaMemcpyDeviceToHost);
-    
-    cufftDestroy(plan);
-    cudaFree(data);
-    return output;
+	normalize_kernel << <(sz + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE >> > ((float*)(data));
+
+	cudaMemcpy(output.data(), data,  sz * sizeof(float), cudaMemcpyDeviceToHost);
+
+	cufftDestroy(plan);
+	cudaFree(data);
+
+	return output;
 }
