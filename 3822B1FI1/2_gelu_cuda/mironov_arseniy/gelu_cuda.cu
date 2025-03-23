460050ee5fa9217d08cc8234eacb949a0b4da10a
Mironov Arseniy <arseniy.mironov.01@mail.ru>
2025-03-23 15:37:22 +0300

    back

--- 3822B1FI1/2_gelu_cuda/mironov_arseniy/gelu_cuda.cu
+++ 3822B1FI1/2_gelu_cuda/mironov_arseniy/gelu_cuda.cu
@@ -1,4 +1,5 @@
 ﻿#include "gelu_cuda.h"
+#include <cmath>
 #include <cuda_runtime.h>
 
 #define PI 0.797884f
@@ -8,38 +9,23 @@ __global__ void GeluKernel(const float* in, float* out, int size) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < size) {
         float x = in[idx];
-        float x_cubed = x * x * x;
-        float tanh_input = PI * (x + 0.044715f * x_cubed);
-        out[idx] = 0.5f * x * (1.f + tanhf(tanh_input));
+        float tanh_in = PI * (x + 0.044715f * x * x * x);
+        out[idx] = 0.5f * x * (1.0f + tanh(tanh_in));
     }
 }
 
 __host__ std::vector<float> GeluCUDA(const std::vector<float>& input) {
     int size = input.size(), memory = size * sizeof(float);
-    int half_size = size >> 1;
     float* in, *out;
 	std::vector<float> result(size);
 
-    cudaStream_t stream1, stream2;
-    cudaStreamCreate(&stream1);
-    cudaStreamCreate(&stream2);
-    
     cudaMalloc(&in, memory);
     cudaMalloc(&out, memory);
 
-    cudaMemcpyAsync(in, input.data(), size * 2, cudaMemcpyHostToDevice, stream1);
-    cudaMemcpyAsync(in, input.data(), size * 2, cudaMemcpyHostToDevice, stream2);
+    cudaMemcpy(in, input.data(), memory, cudaMemcpyKind::cudaMemcpyHostToDevice);
+    GeluKernel<<<(size + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE>>> (in, out, size);
+    cudaMemcpy(result.data(), out, memory, cudaMemcpyKind::cudaMemcpyDeviceToHost);
 
-    GeluKernel<<<(size + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE, 0, stream1>>> (in, out, half_size);
-    GeluKernel<<<(size + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE, 0, stream2>>> (in + half_size, out + half_size, half_size);
-    
-    cudaMemcpyAsync(result.data(), out, half_size * sizeof(float), cudaMemcpyDeviceToHost, stream1);
-    cudaMemcpyAsync(result.data() + half_size, out + half_size, half_size * sizeof(float), cudaMemcpyDeviceToHost, stream2);
-
-    cudaStreamSynchronize(stream1);
-    cudaStreamSynchronize(stream2);
-    cudaStreamDestroy(stream1);
-    cudaStreamDestroy(stream2);
     cudaFree(in);
     cudaFree(out);
 
