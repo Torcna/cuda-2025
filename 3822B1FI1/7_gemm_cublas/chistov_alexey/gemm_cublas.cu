5d08e40ef1ca35595b525e922ec655b1c3ea1b96
Alexey Chistov <alexey.chistov10@gmail.com>
2025-03-23 19:50:58 +0300

    fix

--- 3822B1FI1/7_gemm_cublas/chistov_alexey/gemm_cublas.cu
+++ 3822B1FI1/7_gemm_cublas/chistov_alexey/gemm_cublas.cu
@@ -1,64 +1,38 @@
 #include "gemm_cublas.h"
-#include <cublas_v2.h>
-#include <cuda.h>
 #include <cuda_runtime.h>
-#include <iostream>
+#include <cublas_v2.h>
 
 std::vector<float> GemmCUBLAS(const std::vector<float>& a,
-                              const std::vector<float>& b, int size) {
-    std::vector<float> result(size * size);
-
-    size_t size = size * size * sizeof(float);
-
+                              const std::vector<float>& b,
+                              int n) {
+    std::vector<float> result(n * n);
+    size_t size = n * n * sizeof(float);
     float* d_a;
     float* d_b;
     float* d_c;
+    float *d_c_transposed;
 
-    if (cudaMalloc(&d_a, size) != cudaSuccess) {
-        std::cerr << "Error allocating device memory for d_a" << std::endl;
-        return result;
-    }
-    if (cudaMalloc(&d_b, size) != cudaSuccess) {
-        std::cerr << "Error allocating device memory for d_b" << std::endl;
-        cudaFree(d_a);
-        return result;
-    }
-    if (cudaMalloc(&d_c, size) != cudaSuccess) {
-        std::cerr << "Error allocating device memory for d_c" << std::endl;
-        cudaFree(d_a);
-        cudaFree(d_b);
-        return result;
-    }
-
-    cudaMemcpy(d_a, a.data(), size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, b.data(), size, cudaMemcpyHostToDevice);
+    cudaMalloc(&d_a, size);
+    cudaMalloc(&d_b, size);
+    cudaMalloc(&d_c, size);
+    cudaMalloc(&d_c_transposed, size);
+    cublasSetMatrix(n, n, sizeof(float), a.data(), n, d_a, n);
+    cublasSetMatrix(n, n, sizeof(float), b.data(), n, d_b, n);
 
     cublasHandle_t handle;
     cublasCreate(&handle);
 
     const float alpha = 1.0f;
     const float beta = 0.0f;
-    cublasSetMathMode(handle, CUBLAS_TF32_TENSOR_OP_MATH);
 
-    cublasStatus_t status = cublasGemmEx(handle,
-                 CUBLAS_OP_N, CUBLAS_OP_N,
-                 size, size, size,
-                 &alpha,
-                 d_b, CUDA_R_32F, size,
-                 d_a, CUDA_R_32F, size,
-                 &beta,
-                 d_c, CUDA_R_32F, size,
-                 CUBLAS_COMPUTE_32F_FAST_16F, CUBLAS_GEMM_DEFAULT);
-
-    if (status != CUBLAS_STATUS_SUCCESS) {
-        std::cerr << "Error during cuBLAS GEMM operation" << std::endl;
-    }
-
-    cudaMemcpy(result.data(), d_c, size, cudaMemcpyDeviceToHost);
+    cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, n, n, n, &alpha, d_a, n, d_b, n, &beta, d_c, n);
+    cublasSgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, n, n, &alpha, d_c, n, &beta, nullptr, n, d_c_transposed, n);
+    cublasGetMatrix(n, n, sizeof(float), d_c_transposed, n, result.data(), n);
 
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
+    cudaFree(d_c_transposed);
     cublasDestroy(handle);
 
     return result;
