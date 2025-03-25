57257c36ab1947d9d22534d654a8407e1de62adb
Alexey Chistov <alexey.chistov10@gmail.com>
2025-03-25 22:56:10 +0300

    fix

--- 3822B1FI1/8_fft_cufft/chistov_alexey/fft_cufft.cu
+++ 3822B1FI1/8_fft_cufft/chistov_alexey/fft_cufft.cu
@@ -1,7 +1,6 @@
 #include "fft_cufft.h"
 #include <cufft.h>
 #include <cuda_runtime.h>
-constexpr int threadsPerBlock = 512;
 
 __global__ void normalize_kernel(float* data, int n, float normalizationFactor) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -11,26 +10,29 @@ __global__ void normalize_kernel(float* data, int n, float normalizationFactor) 
 }
 
 std::vector<float> FffCUFFT(const std::vector<float>& input, int batch) {
-    int totalSize = input.size();
-    int fftSize = totalSize / (2 * batch);
-    int byteSize = totalSize * sizeof(cufftComplex);
+    int totalSize  = input.size();
+    int n = (totalSize  / batch) >> 1;
 
-    cufftComplex* d_input;
-    cudaMalloc(&d_input, byteSize);
-    cudaMemcpy(d_input, input.data(), byteSize, cudaMemcpyHostToDevice);
-
+    int byteSize  = sizeof(cufftComplex) * n * batch;
     cufftHandle fftPlan;
-    cufftPlan1d(&fftPlan, fftSize, CUFFT_C2C, batch);
+    cufftPlan1d(&fftPlan, n, CUFFT_C2C, batch);
+    cufftComplex* d_input;
 
+    cudaMalloc(&d_input, byteSize );
+    cudaMemcpy(d_input, input.data(), byteSize , cudaMemcpyHostToDevice);
     cufftExecC2C(fftPlan, d_input, d_input, CUFFT_FORWARD);
     cufftExecC2C(fftPlan, d_input, d_input, CUFFT_INVERSE);
+    cudaDeviceProp deviceProp;
 
-    int blocksPerGrid = (totalSize + threadsPerBlock - 1) / threadsPerBlock;
-    float normalizationFactor = 1.0f / static_cast<float>(fftSize);
-    normalize_kernel<<<blocksPerGrid, threadsPerBlock>>>(reinterpret_cast<float*>(d_input), totalSize, normalizationFactor);
-
+    cudaGetDeviceProperties(&deviceProp, 0);
+    size_t threadsPerBlock = deviceProp.maxThreadsPerBlock;
+    size_t blocksPerGrid = (totalSize  + threadsPerBlock - 1) / threadsPerBlock;
+    float norm = 1.0f / static_cast<float>(n);
+    normalize_kernel<<<blocksPerGrid, threadsPerBlock>>>(
+        reinterpret_cast<float*>(d_input), totalSize , norm);
+    
     std::vector<float> result(totalSize);
-    cudaMemcpy(result.data(), d_input, byteSize, cudaMemcpyDeviceToHost);
+    cudaMemcpy(result.data(), d_input, byteSize , cudaMemcpyDeviceToHost);
 
     cufftDestroy(fftPlan);
     cudaFree(d_input);
