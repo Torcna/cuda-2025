7299f4f7c6bc9dfbef48e2f1b10ba6413e597f10
m1likus <m1likus@yandex.ru>
2025-04-12 15:19:47 +0300

    parallel:

--- 3822B1FI1/1_gelu_omp/kabalova_valeria/gelu_omp.cpp
+++ 3822B1FI1/1_gelu_omp/kabalova_valeria/gelu_omp.cpp
@@ -1,18 +1,16 @@
 #include "gelu_omp.h"
 #include "omp.h"
 #include <cmath>
-#include <functional>
-#include <numbers>
 
-const double arg = std::numbers::sqrt2 * std::numbers::inv_sqrtpi;
+const float arg = 0.7978845608028653558f; //sollya
 
-const std::function<float(float)> Gelu = [](const float x) {
-  return 0.5f * x * (1.0f + std::tanh(*reinterpret_cast<const float*>(&arg) * (x + 0.044715f * x * x * x)));
-  };
+float Gelu(const float& x) {
+  return 0.5f * x * (1.0f + std::tanh(arg * (x + 0.044715f * x * x * x)));
+}
 
 AlignedVector GeluOMP(const AlignedVector& input) {
   AlignedVector resultVector(input.size());
-
+#pragma omp parallel for 
   for (size_t i = 0; i < input.size(); ++i) {
     resultVector[i] = Gelu(input[i]);
   }
