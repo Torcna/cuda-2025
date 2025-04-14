d5115f16b61463407542609c2c0cdc93e98a1c60
m1likus <m1likus@yandex.ru>
2025-04-14 14:48:03 +0300

    back

--- 3822B1FI1/1_gelu_omp/kabalova_valeria/gelu_omp.cpp
+++ 3822B1FI1/1_gelu_omp/kabalova_valeria/gelu_omp.cpp
@@ -1,21 +1,9 @@
 #include "gelu_omp.h"
 #include "omp.h"
 #include <cmath>
-#include <cstdint>
 
 float Gelu(const float& x) {
-  //0.5f * x * (1.0f + std::tanh(coeff3 * (x + 0.044715f * x * x * x)))
-  const float coeff1 = 0.5f;
-  const float coeff2 = 0.044715f;
-  const float coeff3 = 0.7978845608028653558f;
-  float x_3 = x * x * x;
-  float tmp1 = std::fmaf(coeff2, x_3, x);
-  tmp1 = coeff3 * tmp1;
-  tmp1 = std::tanhf(tmp1) + 1.0f;
-  uint32_t tmp2 = *reinterpret_cast<const uint32_t*>(&x);
-  tmp2 = tmp2 >> 1; //0.5f * x
-  float tmp3 = *reinterpret_cast<float*>(&tmp2);
-  return tmp3 * tmp1;
+  return 0.5f * x * (1.0f + std::tanh(coeff3 * (x + 0.044715f * x * x * x)));
 }
 
 AlignedVector GeluOMP(const AlignedVector& input) {
