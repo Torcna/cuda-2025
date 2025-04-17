dd7e45549a8b06bb6ffbd18063da9da56b5994d1
AnastasiaRezantseva <rezantseva.nastya49@gmail.com>
2025-04-17 17:43:45 +0300

    lab 5

---
+++ 3822B1FI1/5_block_gemm_omp/rezantseva_anastasia/block_gemm_omp.cpp
@@ -0,0 +1,40 @@
+#include "block_gemm_omp.h"
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <omp.h>
+#include <random>
+#include <vector>
+
+const int BLOCK_SIZE = 64;
+std::vector<float> BlockGemmOMP(const std::vector<float>& a,
+                                const std::vector<float>& b,
+                                int n) {
+    std::vector<float> b_trans(n * n);
+#pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            b_trans[j * n + i] = b[i * n + j];
+        }
+    }
+
+    std::vector<float> c(n * n, 0.0f);
+
+#pragma omp parallel for collapse(2) schedule(guided)
+    for (int ii = 0; ii < n; ii += BLOCK_SIZE) {
+        for (int jj = 0; jj < n; jj += BLOCK_SIZE) {
+            for (int i = ii; i < std::min(ii + BLOCK_SIZE, n); i++) {
+                for (int j = jj; j < std::min(jj + BLOCK_SIZE, n); j++) {
+                    float sum = 0.0f;
+#pragma omp simd reduction(+:sum)
+                    for (int k = 0; k < n; k++) {
+                        sum += a[i * n + k] * b_trans[j * n + k];
+                    }
+                    c[i * n + j] = sum;
+                }
+            }
+        }
+    }
+
+    return c;
+}
