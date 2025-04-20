40b518fd443ceed1620e5644d6590d673e3dd979
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-04-20 16:33:17 +0300

    1_gelu_omp task

---
+++ 3822B1FI1/1_gelu_omp/sedova_olga/gelu_omp.cpp
@@ -0,0 +1,14 @@
+#include "sedova_olga/gelu_omp.h"
+#include <omp.h>
+#include <cmath>
+
+AlignedVector GeluOMP(const AlignedVector& input) {
+    AlignedVector result(input.size());
+    #pragma omp parallel for
+    for (size_t i = 0; i < input.size(); ++i) {
+        float x = input[i];
+        result[i] = 0.5f * x * (1.0f + std::tanh(std::sqrt(2.0f / 3.14159265f) *
+            (x + 0.044715f * std::pow(x, 3))));
+    }
+    return result;
+}
