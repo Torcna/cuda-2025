30cadb21363b02d331e3972a550aaf654afaf6d5
root <root@MSI>
2025-04-22 00:25:24 +0300

    1_gelu_omp_Suvorov_1_ver

---
+++ 3822B1FI1/1_gelu_omp/suvorov_dmitrii/gelu_omp.cpp
@@ -0,0 +1,19 @@
+#include <omp.h>
+#include <cmath>
+#include "gelu_omp.h"
+
+float Gelu(float x){
+  AlignedVector result(input.size());
+
+#pragma omp parallel for schedule(static)
+  for (int i = 0; i < static_cast<int>(input.size()); ++i) {
+    float x = input[i];
+    float x2 = x * x;
+    float x3 = x2 * x;
+    float inner = 0.797885f * std::fma(x3, 0.044715f, x);
+    float tanh_val = std::tanh(inner);
+    result[i] = 0.5f * x * (1.0f + tanh_val);
+  }
+
+  return result;
+}
