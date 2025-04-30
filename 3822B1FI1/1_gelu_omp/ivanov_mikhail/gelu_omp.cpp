4543fb85e62627bb63d3c5037fe381ba2b364c0a
Mike Ivanov <misha.ivanov.ig@gmail.com>
2025-04-30 17:57:01 +0300

    upload: lab1
    Current version 1.0
    
    Signed-off-by: Mike Ivanov <misha.ivanov.ig@gmail.com>

---
+++ 3822B1FI1/1_gelu_omp/ivanov_mikhail/gelu_omp.cpp
@@ -0,0 +1,16 @@
+#define _USE_MATH_DEFINES
+#include "gelu_omp.h"
+#include <cmath>
+#include <omp.h>
+
+AlignedVector GeluOMP(const AlignedVector& input) {
+  size_t size = input.size();
+  AlignedVector res(size);
+  float var1 = M_SQRT1_2 * M_2_SQRTPI;
+  #pragma omp parallel for
+  for (size_t i = 0; i < size; ++i) {
+    float x = input[i];
+    res[i] = 0.5f * x * (1.f + std::tanh(var1 * (x + 0.044715f * (x * x * x))));
+  }
+  return res;
+}
