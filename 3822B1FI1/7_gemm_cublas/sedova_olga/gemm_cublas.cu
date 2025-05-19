a8c78fe68a8a514f7f4098ba543a149b1bb0a12f
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2025-05-19 21:40:57 +0300

    7

--- 3822B1FI1/7_gemm_cublas/sedova_olga/gemm_cublas.cu
+++ 3822B1FI1/7_gemm_cublas/sedova_olga/gemm_cublas.cu
@@ -2,7 +2,29 @@
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 #include <stdexcept>
-#include <vector>
+#include <iostream>
+
+#define CUDA_CHECK(call)                                                    
+    do {                                                                    
+        cudaError_t err = call;                                             
+        if (err != cudaSuccess) {                                           
+            std::cerr << "CUDA error in " << __FILE__ << ":" << __LINE__    
+                      << ", code: " << err << ", reason: "                  
+                      << cudaGetErrorString(err) << std::endl;              
+            throw std::runtime_error("CUDA call failed");                   
+        }                                                                   
+    } while (0)
+
+#define CUBLAS_CHECK(call)                                                  
+    do {                                                                    
+        cublasStatus_t status = call;                                       
+        if (status != CUBLAS_STATUS_SUCCESS) {                             
+            std::cerr << "cuBLAS error in " << __FILE__ << ":" << __LINE__ 
+                      << ", code: " << status << std::endl;                 
+            throw std::runtime_error("cuBLAS call failed");                 
+        }                                                                   
+    } while (0)
+
 std::vector<float> GemmCUBLAS(const std::vector<float>& a,
                               const std::vector<float>& b,
                               int n) {
@@ -12,42 +34,46 @@ std::vector<float> GemmCUBLAS(const std::vector<float>& a,
     CUBLAS_CHECK(cublasCreate(&handle));
 
     size_t size = n * n * sizeof(float);
-    float *d_a = nullptr, *d_b = nullptr, *d_c = nullptr;
+    std::vector<float> c(n * n);
+
+    float *d_a = nullptr, *d_b = nullptr, *d_c = nullptr, *d_ct = nullptr;
 
     CUDA_CHECK(cudaMalloc(&d_a, size));
     CUDA_CHECK(cudaMalloc(&d_b, size));
     CUDA_CHECK(cudaMalloc(&d_c, size));
+    CUDA_CHECK(cudaMalloc(&d_ct, size));
 
-    CUDA_CHECK(cudaMemcpy(d_a, a.data(), size, cudaMemcpyHostToDevice));
-    CUDA_CHECK(cudaMemcpy(d_b, b.data(), size, cudaMemcpyHostToDevice));
+    CUBLAS_CHECK(cublasSetMatrix(n, n, sizeof(float), a.data(), n, d_a, n));
+    CUBLAS_CHECK(cublasSetMatrix(n, n, sizeof(float), b.data(), n, d_b, n));
 
     const float alpha = 1.0f;
     const float beta = 0.0f;
 
-    cublasStatus_t stat = cublasSgemm(handle,
-                                     CUBLAS_OP_N, CUBLAS_OP_N,
-                                     n, n, n,
-                                     &alpha,
-                                     d_b, n,
-                                     d_a, n,
-                                     &beta,
-                                     d_c, n);
+    CUBLAS_CHECK(cublasSgemm(handle,
+                             CUBLAS_OP_T, CUBLAS_OP_T,
+                             n, n, n,
+                             &alpha,
+                             d_b, n,
+                             d_a, n,
+                             &beta,
+                             d_c, n));
 
-    if (stat != CUBLAS_STATUS_SUCCESS) {
-        cudaFree(d_a);
-        cudaFree(d_b);
-        cudaFree(d_c);
-        cublasDestroy(handle);
-        throw std::runtime_error("cuBLAS sgemm failed");
-    }
+     CUBLAS_CHECK(cublasSgemm(handle,
+                             CUBLAS_OP_T, CUBLAS_OP_N,
+                             n, n, &alpha,
+                             d_c, n,
+                             &beta,
+                             d_a, n,
+                             d_ct, n));
 
-    std::vector<float> c(n * n);
-    CUDA_CHECK(cudaMemcpy(c.data(), d_c, size, cudaMemcpyDeviceToHost));
 
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
-    cublasDestroy(handle);
+    CUBLAS_CHECK(cublasGetMatrix(n, n, sizeof(float), d_ct, n, c.data(), n));
+
+    CUDA_CHECK(cudaFree(d_a));
+    CUDA_CHECK(cudaFree(d_b));
+    CUDA_CHECK(cudaFree(d_c));
+     CUDA_CHECK(cudaFree(d_ct));
+    CUBLAS_CHECK(cublasDestroy(handle));
 
     return c;
 }
