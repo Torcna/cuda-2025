71fc1fc8759e380882c16cc21d653d4ecb09c499
MaxikGuy <S_Maksssim@mail.ru>
2025-05-19 13:54:51 +0300

    lab1 fix

--- 3822B1FI1/1_gelu_omp/savchenko_maxim/gelu_omp.cpp
+++ 3822B1FI1/1_gelu_omp/savchenko_maxim/gelu_omp.cpp
@@ -6,7 +6,7 @@ AlignedVector GeluOMP(const AlignedVector& input) {
     AlignedVector output(input.size());
 	
 	const float c1 = 0.7978845608f; // std::sqrt(2.0f / M_PI) 
-	const float c2 = 044715f;
+	const float c2 = 0.044715f;
 	
     #pragma omp parallel for
     for (size_t i = 0; i < input.size(); ++i) {
