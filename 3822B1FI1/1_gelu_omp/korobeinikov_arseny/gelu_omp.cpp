f8d6aee0f1d061aca528a0f80a95f8d650f8e673
Arseny-Korobeinikov <arsenykorob@gmail.com>
2025-05-20 19:38:58 +0300

    fix 1-2 lab

--- 3822B1FI1/1_gelu_omp/korobeinikov_arseny/gelu_omp.cpp
+++ 3822B1FI1/1_gelu_omp/korobeinikov_arseny/gelu_omp.cpp
@@ -2,48 +2,17 @@
 #include "gelu_omp.h"
 #include <cmath>
 #include <omp.h>
-#include <algorithm>
-
-namespace
-{
-    constexpr float GELU_COEF = 0.044715f;
-    constexpr float SQRT_2_OVER_PI = 0.7978845608f;
-
-    inline float fast_gelu_approx(float x) noexcept
-    {
-
-        const float x_cubed = x * x * x;
-        const float inner = SQRT_2_OVER_PI * (x + GELU_COEF * x_cubed);
-
-        if (inner > 4.0f)
-            return x;
-        if (inner < -4.0f)
-            return 0.0f;
-
-        const float tanh_val = 1.0f - 2.0f / (1.0f + std::exp(2.0f * inner));
-        return 0.5f * x * (1.0f + tanh_val);
-    }
-}
 
 AlignedVector GeluOMP(const AlignedVector &input)
 {
-    if (input.empty())
-    {
-        return {};
-    }
-
-    const size_t size = input.size();
+    size_t size = input.size();
     AlignedVector res(size);
-    const float *in_data = input.data();
-    float *out_data = res.data();
-
-    const size_t block_size = std::max<size_t>(256, size / (4 * omp_get_max_threads()));
-
-#pragma omp parallel for schedule(dynamic, block_size)
+    float var1 = M_SQRT1_2 * M_2_SQRTPI;
+#pragma omp parallel for
     for (size_t i = 0; i < size; ++i)
     {
-        out_data[i] = fast_gelu_approx(in_data[i]);
+        float x = input[i];
+        res[i] = 0.5f * x * (1.f + std::tanh(var1 * (x + 0.044715f * (x * x * x))));
     }
-
     return res;
 }
