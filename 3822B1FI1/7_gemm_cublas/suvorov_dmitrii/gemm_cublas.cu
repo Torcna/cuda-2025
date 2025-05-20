187b8403b58962a97e0d7e567cd17109e37556b4
suvorovDm-1 <dim-suv@mail.ru>
2025-05-20 13:55:55 +0300

    fixed 7 task

--- 3822B1FI1/7_gemm_cublas/suvorov_dmitrii/gemm_cublas.cu
+++ 3822B1FI1/7_gemm_cublas/suvorov_dmitrii/gemm_cublas.cu
@@ -1,35 +1,57 @@
 #include "gemm_cublas.h"
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
 
-std::vector<float> GemmCUBLAS(const std::vector<float>& matrix_a, const std::vector<float>& matrix_b, int dimension) {
-  int buffer_size = dimension * dimension * sizeof(float);
+std::vector<float> GemmCUBLAS(const std::vector<float>& matrix_a,
+                               const std::vector<float>& matrix_b,
+                               int dimension) {
   std::vector<float> matrix_result(dimension * dimension);
+  size_t buffer_size = dimension * dimension * sizeof(float);
 
   float *device_matrix_a, *device_matrix_b, *device_result, *device_result_transposed;
+
   cudaMalloc(&device_matrix_a, buffer_size);
   cudaMalloc(&device_matrix_b, buffer_size);
   cudaMalloc(&device_result, buffer_size);
   cudaMalloc(&device_result_transposed, buffer_size);
 
+  cublasSetMatrix(dimension, dimension, sizeof(float),
+                  matrix_a.data(), dimension,
+                  device_matrix_a, dimension);
+  cublasSetMatrix(dimension, dimension, sizeof(float),
+                  matrix_b.data(), dimension,
+                  device_matrix_b, dimension);
+
   cublasHandle_t cublas_handle;
   cublasCreate(&cublas_handle);
 
-  cublasSetMatrix(dimension, dimension, sizeof(float), matrix_a.data(), dimension, device_matrix_a, dimension);
-  cublasSetMatrix(dimension, dimension, sizeof(float), matrix_b.data(), dimension, device_matrix_b, dimension);
+  const float alpha = 1.0f;
+  const float beta = 0.0f;
 
-  float alpha = 1.0f;
-  float beta = 0.0f;
+  cublasSgemm(cublas_handle, CUBLAS_OP_T, CUBLAS_OP_T,
+              dimension, dimension, dimension,
+              &alpha,
+              device_matrix_a, dimension,
+              device_matrix_b, dimension,
+              &beta,
+              device_result, dimension);
 
-  cublasSgemm(cublas_handle, CUBLAS_OP_T, CUBLAS_OP_T, dimension, dimension, dimension, &alpha, device_matrix_a, dimension, device_matrix_b, dimension, &beta, device_result, dimension);
-  cublasSgeam(cublas_handle, CUBLAS_OP_T, CUBLAS_OP_N, dimension, dimension, &alpha, device_result, dimension, &beta, nullptr, dimension, device_result_transposed, dimension);
+  cublasSgeam(cublas_handle, CUBLAS_OP_T, CUBLAS_OP_N,
+              dimension, dimension,
+              &alpha, device_result, dimension,
+              &beta, nullptr, dimension,
+              device_result_transposed, dimension);
 
-  cublasGetMatrix(dimension, dimension, sizeof(float), device_result_transposed, dimension, matrix_result.data(), dimension);
-
-  cublasDestroy(cublas_handle);
+  cublasGetMatrix(dimension, dimension, sizeof(float),
+                  device_result_transposed, dimension,
+                  matrix_result.data(), dimension);
 
   cudaFree(device_matrix_a);
   cudaFree(device_matrix_b);
   cudaFree(device_result);
   cudaFree(device_result_transposed);
+
+  cublasDestroy(cublas_handle);
 
   return matrix_result;
 }
