a0c74b4777697019ad65469c7ef939745501daea
Baranov Aleksey <all.baranov@mail.ru>
2025-06-02 21:12:48 +0300

    one more fix

--- 3822B1FI1/9_gelu_ocl/baranov_aleksey/gelu_ocl.cpp
+++ 3822B1FI1/9_gelu_ocl/baranov_aleksey/gelu_ocl.cpp
@@ -1,54 +1,62 @@
 #include "gelu_ocl.h"
 #include <CL/cl.h>
-#include <cmath>
-#include <memory>
-#include <iostream>
-#include <vector>
-#include <string>
 
-std::vector<float> GeluOCL(const std::vector<float>& input) {
-    std::string kernelSource =R"(
-    __kernel void Kernel(__global const float* input, __global float* output, int size) {
-        int index = get_global_id(0);
-        if (index < size) {
-            const float value = input[index];
-            output[index] = value / (1.0f + exp(-1.59577f * (value + 0.044715f * value * value * value)));
+
+const char* geluKernelSource = R"(
+    __kernel void GeluKernel(__global const float* input, __global float* output, const int n) {
+        int idx = get_global_id(0);
+        if (idx < n) {
+            float x = input[idx];
+            output[idx] = 0.5f * x * (1.0f + tanh(0.797884f * (x + 0.044715f * (x * x * x))));
         }
     }
     )";
 
-    std::vector<cl::Platform> platforms;
-    cl::Platform::get(&platforms);
-    cl::Platform selectedPlatform = platforms.front();
 
-    std::vector<cl::Device> devices;
-    selectedPlatform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
-    cl::Device selectedDevice = devices.front();
+std::vector<float> GeluOCL(const std::vector<float>& inputVec) {
+    int localSize = 256;
+    const size_t n = inputVec.size(), memSize = n * sizeof(float);
+    std::vector<float> outputVec(n);
 
-    cl::Context context(selectedDevice);
-    cl::CommandQueue queue(context);
+    cl_platform_id clPlatform;
+    cl_device_id clDevice;
+    cl_context clCtx;
+    cl_command_queue clQueue;
+    cl_program clProg;
+    cl_kernel clKernel;
+    cl_mem bufInput, bufOutput;
     
-    cl::Program::Sources sources;
-    sources.emplace_back(kernelSource);
-    cl::Program program(context, sources);
-    program.build();
+    clGetPlatformIDs(1, &clPlatform, NULL);
+    clGetDeviceIDs(clPlatform, CL_DEVICE_TYPE_GPU, 1, &clDevice, NULL);
 
-    cl::Kernel kernel(program, "Kernel");
-    size_t inputSize = input.size();
-    size_t bufferSize = inputSize * sizeof(float);
+    clCtx = clCreateContext(NULL, 1, &clDevice, NULL, NULL, NULL);
+    clQueue = clCreateCommandQueue(clCtx, clDevice, 0, NULL);
+       
+    clProg = clCreateProgramWithSource(clCtx, 1, &geluKernelSource, NULL, NULL);
+    const char* buildOpts = "-cl-fast-relaxed-math";
+    clBuildProgram(clProg, 1, &clDevice, buildOpts, NULL, NULL);
+    clKernel = clCreateKernel(clProg, "GeluKernel", NULL);
 
-    cl::Buffer inputBuffer(context, CL_MEM_READ_ONLY, bufferSize);
-    cl::Buffer outputBuffer(context, CL_MEM_WRITE_ONLY, bufferSize);
+    bufInput = clCreateBuffer(clCtx, CL_MEM_READ_ONLY, memSize, NULL, NULL);
+    bufOutput = clCreateBuffer(clCtx, CL_MEM_WRITE_ONLY, memSize, NULL, NULL);
+
+    clEnqueueWriteBuffer(clQueue, bufInput, CL_TRUE, 0, memSize, inputVec.data(), 0, NULL, NULL);
     
-    queue.enqueueWriteBuffer(inputBuffer, CL_TRUE, 0, bufferSize, input.data());
-    kernel.setArg(0, inputBuffer);
-    kernel.setArg(1, outputBuffer);
-    kernel.setArg(2, static_cast<int>(inputSize));
-    
-    queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(inputSize), cl::NullRange);
-    
-    std::vector<float> output(inputSize);
-    queue.enqueueReadBuffer(outputBuffer, CL_TRUE, 0, bufferSize, output.data());
-    
-    return output;
+    clSetKernelArg(clKernel, 0, sizeof(cl_mem), &bufInput);
+    clSetKernelArg(clKernel, 1, sizeof(cl_mem), &bufOutput);
+    clSetKernelArg(clKernel, 2, sizeof(int), &n);
+
+    const size_t globalSize = (n + localSize - 1) / localSize * localSize;
+    const size_t localWorkSize = localSize;  
+    clEnqueueNDRangeKernel(clQueue, clKernel, 1, NULL, &globalSize, &localWorkSize, 0, NULL, NULL);
+    clEnqueueReadBuffer(clQueue, bufOutput, CL_TRUE, 0, memSize, outputVec.data(), 0, NULL, NULL);
+
+    clReleaseMemObject(bufInput);
+    clReleaseMemObject(bufOutput);
+    clReleaseKernel(clKernel);
+    clReleaseProgram(clProg);
+    clReleaseCommandQueue(clQueue);
+    clReleaseContext(clCtx);
+
+    return outputVec;
 }
